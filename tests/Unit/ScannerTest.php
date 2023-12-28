088bc0715427489afae044cc8c94b300be6ebc03
Max Weber <mweber@kzvnr.de>
2023-12-28 21:45:20 +0100

    refactored ScannerTest for better maintainability

--- tests/Unit/ScannerTest.php
+++ tests/Unit/ScannerTest.php
@@ -48,241 +48,67 @@ it('parses sourcecode to tokens', function () {
 
     $tokens = $scanner->scanTokens($source);
 
-    expect(array_shift($tokens))
-        ->toHaveType(TokenType::LEFT_PAREN)
-        ->toHaveLexeme("(")
-        ->toHaveLiteral(null)
-        ->toHaveLine(3);
+    $line           = 3;
+    $expectedTokens = [
+        ['type' => TokenType::LEFT_PAREN, 'lexeme' => "(", 'literal' => null, 'line' => $line],
+        ['type' => TokenType::RIGHT_PAREN, 'lexeme' => ")", 'literal' => null, 'line' => $line],
+        ['type' => TokenType::LEFT_BRACE, 'lexeme' => "{", 'literal' => null, 'line' => $line],
+        ['type' => TokenType::RIGHT_BRACE, 'lexeme' => "}", 'literal' => null, 'line' => $line],
+        ['type' => TokenType::COMMA, 'lexeme' => ",", 'literal' => null, 'line' => $line],
+        ['type' => TokenType::DOT, 'lexeme' => ".", 'literal' => null, 'line' => $line],
+        ['type' => TokenType::MINUS, 'lexeme' => "-", 'literal' => null, 'line' => $line],
+        ['type' => TokenType::PLUS, 'lexeme' => "+", 'literal' => null, 'line' => $line],
+        ['type' => TokenType::SEMICOLON, 'lexeme' => ";", 'literal' => null, 'line' => $line],
+        ['type' => TokenType::STAR, 'lexeme' => "*", 'literal' => null, 'line' => $line],
+        ['type' => TokenType::SLASH, 'lexeme' => "/", 'literal' => null, 'line' => $line]
+    ];
+    $line           = 6;
+    $expectedTokens = array_merge($expectedTokens, [
+        ['type' => TokenType::BANG, 'lexeme' => "!", 'literal' => null, 'line' => $line],
+        ['type' => TokenType::BANG_EQUAL, 'lexeme' => "!=", 'literal' => null, 'line' => $line],
+        ['type' => TokenType::EQUAL, 'lexeme' => "=", 'literal' => null, 'line' => $line],
+        ['type' => TokenType::EQUAL_EQUAL, 'lexeme' => "==", 'literal' => null, 'line' => $line],
+        ['type' => TokenType::GREATER, 'lexeme' => ">", 'literal' => null, 'line' => $line],
+        ['type' => TokenType::GREATER_EQUAL, 'lexeme' => ">=", 'literal' => null, 'line' => $line],
+        ['type' => TokenType::LESS, 'lexeme' => "<", 'literal' => null, 'line' => $line],
+        ['type' => TokenType::LESS_EQUAL, 'lexeme' => "<=", 'literal' => null, 'line' => $line]
+    ]);
+    $line           = 10;
+    $expectedTokens = array_merge($expectedTokens, [
+        ['type' => TokenType::STRING, 'lexeme' => '"Lorem Ipsum"', 'literal' => 'Lorem Ipsum', 'line' => $line],
+        ['type' => TokenType::NUMBER, 'lexeme' => '13.37', 'literal' => 13.37, 'line' => $line]
+    ]);
+    $line           = 19;
+    $expectedTokens = array_merge($expectedTokens, [
+        ['type' => TokenType::IDENTIFIER, 'lexeme' => '_identifier_', 'literal' => null, 'line' => $line],
+        ['type' => TokenType::AND, 'lexeme' => 'and', 'literal' => null, 'line' => $line],
+        ['type' => TokenType::CLS, 'lexeme' => 'class', 'literal' => null, 'line' => $line],
+        ['type' => TokenType::ELSE, 'lexeme' => 'else', 'literal' => null, 'line' => $line],
+        ['type' => TokenType::FALSE, 'lexeme' => 'false', 'literal' => null, 'line' => $line],
+        ['type' => TokenType::FUN, 'lexeme' => 'fun', 'literal' => null, 'line' => $line],
+        ['type' => TokenType::FOR, 'lexeme' => 'for', 'literal' => null, 'line' => $line],
+        ['type' => TokenType::IF, 'lexeme' => 'if', 'literal' => null, 'line' => $line],
+        ['type' => TokenType::NIL, 'lexeme' => 'nil', 'literal' => null, 'line' => $line],
+        ['type' => TokenType::OR, 'lexeme' => 'or', 'literal' => null, 'line' => $line],
+        ['type' => TokenType::PRINT, 'lexeme' => 'print', 'literal' => null, 'line' => $line],
+        ['type' => TokenType::RETURN, 'lexeme' => 'return', 'literal' => null, 'line' => $line],
+        ['type' => TokenType::SUPER, 'lexeme' => 'super', 'literal' => null, 'line' => $line],
+        ['type' => TokenType::THIS, 'lexeme' => 'this', 'literal' => null, 'line' => $line],
+        ['type' => TokenType::TRUE, 'lexeme' => 'true', 'literal' => null, 'line' => $line],
+        ['type' => TokenType::VAR, 'lexeme' => 'var', 'literal' => null, 'line' => $line],
+        ['type' => TokenType::WHILE, 'lexeme' => 'while', 'literal' => null, 'line' => $line]
+    ]);
+    $expectedTokens = array_merge($expectedTokens, [
+        ['type' => TokenType::EOF, 'lexeme' => '', 'literal' => null, 'line' => 20],
+    ]);
 
-    expect(array_shift($tokens))
-        ->toHaveType(TokenType::RIGHT_PAREN)
-        ->toHaveLexeme(")")
-        ->toHaveLiteral(null)
-        ->toHaveLine(3);
-
-    expect(array_shift($tokens))
-        ->toHaveType(TokenType::LEFT_BRACE)
-        ->toHaveLexeme("{")
-        ->toHaveLiteral(null)
-        ->toHaveLine(3);
-
-    expect(array_shift($tokens))
-        ->toHaveType(TokenType::RIGHT_BRACE)
-        ->toHaveLexeme("}")
-        ->toHaveLiteral(null)
-        ->toHaveLine(3);
-
-    expect(array_shift($tokens))
-        ->toHaveType(TokenType::COMMA)
-        ->toHaveLexeme(",")
-        ->toHaveLiteral(null)
-        ->toHaveLine(3);
-
-    expect(array_shift($tokens))
-        ->toHaveType(TokenType::DOT)
-        ->toHaveLexeme(".")
-        ->toHaveLiteral(null)
-        ->toHaveLine(3);
-
-    expect(array_shift($tokens))
-        ->toHaveType(TokenType::MINUS)
-        ->toHaveLexeme("-")
-        ->toHaveLiteral(null)
-        ->toHaveLine(3);
-
-    expect(array_shift($tokens))
-        ->toHaveType(TokenType::PLUS)
-        ->toHaveLexeme("+")
-        ->toHaveLiteral(null)
-        ->toHaveLine(3);
-
-    expect(array_shift($tokens))
-        ->toHaveType(TokenType::SEMICOLON)
-        ->toHaveLexeme(";")
-        ->toHaveLiteral(null)
-        ->toHaveLine(3);
-
-    expect(array_shift($tokens))
-        ->toHaveType(TokenType::STAR)
-        ->toHaveLexeme("*")
-        ->toHaveLiteral(null)
-        ->toHaveLine(3);
-
-    expect(array_shift($tokens))
-        ->toHaveType(TokenType::SLASH)
-        ->toHaveLexeme("/")
-        ->toHaveLiteral(null)
-        ->toHaveLine(3);
-
-
-    expect(array_shift($tokens))
-        ->toHaveType(TokenType::BANG)
-        ->toHaveLexeme("!")
-        ->toHaveLiteral(null)
-        ->toHaveLine(6);
-
-    expect(array_shift($tokens))
-        ->toHaveType(TokenType::BANG_EQUAL)
-        ->toHaveLexeme("!=")
-        ->toHaveLiteral(null)
-        ->toHaveLine(6);
-
-    expect(array_shift($tokens))
-        ->toHaveType(TokenType::EQUAL)
-        ->toHaveLexeme("=")
-        ->toHaveLiteral(null)
-        ->toHaveLine(6);
-
-    expect(array_shift($tokens))
-        ->toHaveType(TokenType::EQUAL_EQUAL)
-        ->toHaveLexeme("==")
-        ->toHaveLiteral(null)
-        ->toHaveLine(6);
-
-    expect(array_shift($tokens))
-        ->toHaveType(TokenType::GREATER)
-        ->toHaveLexeme(">")
-        ->toHaveLiteral(null)
-        ->toHaveLine(6);
-
-    expect(array_shift($tokens))
-        ->toHaveType(TokenType::GREATER_EQUAL)
-        ->toHaveLexeme(">=")
-        ->toHaveLiteral(null)
-        ->toHaveLine(6);
-
-    expect(array_shift($tokens))
-        ->toHaveType(TokenType::LESS)
-        ->toHaveLexeme("<")
-        ->toHaveLiteral(null)
-        ->toHaveLine(6);
-
-    expect(array_shift($tokens))
-        ->toHaveType(TokenType::LESS_EQUAL)
-        ->toHaveLexeme("<=")
-        ->toHaveLiteral(null)
-        ->toHaveLine(6);
-
-    expect(array_shift($tokens))
-        ->toHaveType(TokenType::STRING)
-        ->toHaveLexeme('"Lorem Ipsum"')
-        ->toHaveLiteral('Lorem Ipsum')
-        ->toHaveLine(10);
-
-    expect(array_shift($tokens))
-        ->toHaveType(TokenType::NUMBER)
-        ->toHaveLexeme('13.37')
-        ->toHaveLiteral(13.37)
-        ->toHaveLine(10);
-
-
-    expect(array_shift($tokens))
-        ->toHaveType(TokenType::IDENTIFIER)
-        ->toHaveLexeme('_identifier_')
-        ->toHaveLiteral(null)
-        ->toHaveLine(19);
-
-    expect(array_shift($tokens))
-        ->toHaveType(TokenType::AND)
-        ->toHaveLexeme('and')
-        ->toHaveLiteral(null)
-        ->toHaveLine(19);
-
-    expect(array_shift($tokens))
-        ->toHaveType(TokenType::CLS)
-        ->toHaveLexeme('class')
-        ->toHaveLiteral(null)
-        ->toHaveLine(19);
-
-    expect(array_shift($tokens))
-        ->toHaveType(TokenType::ELSE)
-        ->toHaveLexeme('else')
-        ->toHaveLiteral(null)
-        ->toHaveLine(19);
-
-    expect(array_shift($tokens))
-        ->toHaveType(TokenType::FALSE)
-        ->toHaveLexeme('false')
-        ->toHaveLiteral(null)
-        ->toHaveLine(19);
-
-    expect(array_shift($tokens))
-        ->toHaveType(TokenType::FUN)
-        ->toHaveLexeme('fun')
-        ->toHaveLiteral(null)
-        ->toHaveLine(19);
-
-    expect(array_shift($tokens))
-        ->toHaveType(TokenType::FOR)
-        ->toHaveLexeme('for')
-        ->toHaveLiteral(null)
-        ->toHaveLine(19);
-
-    expect(array_shift($tokens))
-        ->toHaveType(TokenType::IF)
-        ->toHaveLexeme('if')
-        ->toHaveLiteral(null)
-        ->toHaveLine(19);
-
-    expect(array_shift($tokens))
-        ->toHaveType(TokenType::NIL)
-        ->toHaveLexeme('nil')
-        ->toHaveLiteral(null)
-        ->toHaveLine(19);
-
-    expect(array_shift($tokens))
-        ->toHaveType(TokenType::OR)
-        ->toHaveLexeme('or')
-        ->toHaveLiteral(null)
-        ->toHaveLine(19);
-
-    expect(array_shift($tokens))
-        ->toHaveType(TokenType::PRINT)
-        ->toHaveLexeme('print')
-        ->toHaveLiteral(null)
-        ->toHaveLine(19);
-
-    expect(array_shift($tokens))
-        ->toHaveType(TokenType::RETURN)
-        ->toHaveLexeme('return')
-        ->toHaveLiteral(null)
-        ->toHaveLine(19);
-
-    expect(array_shift($tokens))
-        ->toHaveType(TokenType::SUPER)
-        ->toHaveLexeme('super')
-        ->toHaveLiteral(null)
-        ->toHaveLine(19);
-
-    expect(array_shift($tokens))
-        ->toHaveType(TokenType::THIS)
-        ->toHaveLexeme('this')
-        ->toHaveLiteral(null)
-        ->toHaveLine(19);
-
-    expect(array_shift($tokens))
-        ->toHaveType(TokenType::TRUE)
-        ->toHaveLexeme('true')
-        ->toHaveLiteral(null)
-        ->toHaveLine(19);
-
-    expect(array_shift($tokens))
-        ->toHaveType(TokenType::VAR)
-        ->toHaveLexeme('var')
-        ->toHaveLiteral(null)
-        ->toHaveLine(19);
-
-    expect(array_shift($tokens))
-        ->toHaveType(TokenType::WHILE)
-        ->toHaveLexeme('while')
-        ->toHaveLiteral(null)
-        ->toHaveLine(19);
-
-    expect(array_shift($tokens))
-        ->toHaveType(TokenType::EOF)
-        ->toHaveLexeme('')
-        ->toHaveLiteral(null)
-        ->toHaveLine(20);
+    foreach ($expectedTokens as $expectedToken) {
+        expect(array_shift($tokens))
+            ->toHaveType($expectedToken['type'])
+            ->toHaveLexeme($expectedToken['lexeme'])
+            ->toHaveLiteral($expectedToken['literal'])
+            ->toHaveLine($expectedToken['line']);
+    }
 
     expect($tokens)->toBeEmpty();
 });
